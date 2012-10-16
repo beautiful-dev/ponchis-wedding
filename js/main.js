6e1436250238eccf7667f98c3239658af8fb03a8
Ivan Valdes (@ivanvc) <ivan@ooyala.com>
2012-10-16 13:21:32 -0500

    Favicon

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ $(function() {
     var sectionHeight = $('section').height();
     var windowHeight = $(window).height();
     var h1Height = $('h1').height();
-    console.log((windowHeight - sectionHeight + h1Height)/2);
     $('section').css('padding-top', (windowHeight - sectionHeight - h1Height)/2);
   };
 
